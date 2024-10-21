1c6754f73eabf5c79bd16b67b76bbf64efcd7f49
vidimak <dumiakmyroslav@dspu.edu.ua>
2024-10-21 14:45:17 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,22 +1,35 @@
 #include <iostream>
+#include <iomanip>
 #include <cmath>
 using namespace std;
 int main()
 {
-	double x;
-	double y;
-	double R;
-	cout << "x = "; cin >> x;
-	cout << "y = "; cin >> y;
-	cout << "R = "; cin >> R;
-
-	if (((x + R) * (x + R) + (y + R) * (y + R) >= R * R &&
-		x <= 0 && x >= -R && y <= 0 && y >= -R) ||
-		((x - R) * (x - R) + y * y <= R * R && y >= 0))
-		cout << "yes" << endl;
-	else
-		cout << "no" << endl;
-
-	cin.get();
+	double x, xp, xk, dx, A, B, y;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << "   |"
+		<< setw(7) << "y" << "    |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		A = 5 * exp(3 * x);
+		if (x <= -1)
+			B = 3 + sin(abs(x));
+		else
+			if (x > 3)
+				B = 7 - sqrt(2 * pow(x , 3));
+			else
+				B = 2 * exp(4 / x - 1);
+		y = A - B;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
 	return 0;
 }
