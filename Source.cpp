1dcbdaf8a9d8e640bc56b24a69b5038e118f2e57
vidimak <dumiakmyroslav@dspu.edu.ua>
2024-10-21 14:43:14 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,34 +1,34 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 int main()
 {
 	double x; 
-	double y; 
-	double A; 
-	//double B; 
+	double a; 
+	double b; 
+	double c; 
+	double F; 
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
 	cout << "x = "; cin >> x;
-	A = 5 * exp(3 * x);
 	// спосіб 1: розгалуження в скороченій формі
-	if (x <= -1)
-		B = 3 + sin(fabs(x) );
-	if (-1 < x && x <= 3)
-		B = 2 * exp((x / 4) - 1);
-	if (x > 3)
-		B = 7 - sqrt(2 * x * x * x);
-	y = A - B;
+	if (c < 0 && b != 0)
+		F = a * x * x + b * b * x;
+	if (c > 0 && b == 0)
+		F = (x - a) / (x - c);
+	if (!(c < 0 && b != 0) && !(c > 0 && b == 0))
+		F = x / c;
 	cout << endl;
-	cout << "1) y = " << y << endl;
+	cout << "1) F = " << F << endl;
 	// спосіб 2: розгалуження в повній формі
-	/*if (x <= -1)
-		B = 3 + sin(fabs(x));
+	if (c < 0 && b != 0)
+		F = a * x * x + b * b * x;
 	else
-		if (x > 3)
-			B = 7 - sqrt(2 * x * x * x);
+		if (c > 0 && b == 0)
+			F = (x - a) / (x - c);
 		else
-			B = 2 * exp((x / 4) - 1);
-	y = A - B;
-	cout << "2) y = " << y << endl;*/
+			F = x / c;
+	cout << "2) F = " << F << endl;
 	cin.get();
 	return 0;
 }
