6aa3fb5dc114b2ccfdbec6c260491cf0445f6f0c
vidimak <dumiakmyroslav@dspu.edu.ua>
2024-10-28 11:01:46 +0200

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,68 +1,44 @@
 #include <iostream>
+#include <iomanip>
 #include <cmath>
 using namespace std;
-int main() {
-	double P, S;
-	int k, i;
-	P = 1;
-	k = 1;
 
-	while (k <= 15)
+int main()
+{
+	double xp, xk, x, dx, eps, a = 0, R = 0, S = 0;
+	int n = 0;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "eps = "; cin >> eps;
+
+	cout << fixed;
+	cout << "-----------------------------------------" << endl;
+	cout << "|" << setw(6) << "x" << "  |"
+		<< setw(10) << "log(1 - x)" << " |"
+		<< setw(7) << "S" << "    |"
+		<< setw(5) << "n" << " |"
+		<< endl;
+	cout << "-----------------------------------------" << endl;
+	x = xp;
+	while (x <= xk)
 	{
-		S = 0;
-		i = 1;
-		while (i <= k)
-		{
-			S += cos(i + k);
-			i++;
-		}
-		P *= pow(1 + cos(S), 2);
-		k++;
+		n = 1;
+		a = x;
+		S = a;
+		do {
+			n++;
+			R = (n - 1) / n * x;
+			a *= R;
+			S += a;
+		} while (abs(a) >= eps);
+		cout << "|" << setw(7) << setprecision(2) << x << " |"
+			<< setw(10) << setprecision(5) << log(1 - x) << " |"
+			<< setw(10) << setprecision(5) << S << " |"
+			<< setw(5) << n << " |"
+			<< endl;
+		x += dx;
 	}
-	cout << P << endl;
-	P = 1;
-	k = 1;
-	do
-	{
-		S = 0;
-		i = 1;
-		do
-		{
-
-			S += cos(i + k);
-			i++;
-		} while (i <= k);
-		P *= pow(1 + cos(S), 2);
-		k++;
-	} 
-	
-	while (k <= 15);
-	cout << P << endl;
-	P = 1;
-	for (k = 1; k <= 15; k++)
-	{
-		S = 0;
-		for (i = 1; i <= k; i++)
-		{
-			S += cos(i + k);
-		}
-		P *= pow(1 + cos(S), 2);
-	}
-	cout << P << endl;
-	P = 1;
-	
-	for (k = 15; k >= 1; k--)
-
-	{
-		S = 0;
-		for (i = 1; i >= k; i--)
-
-		{
-			S += cos(i + k);
-
-		}
-		P *= pow(1 + cos(S), 2);
-	}
-	cout << P << endl;
+	cout << "-----------------------------------------" << endl;
 	return 0;
 }
